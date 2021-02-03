1b70e3df18ef7459295e9d16ca325296dda06d77
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2021-02-02 23:34:50 -0300

    Adicionado update em tasks.

--- tasks/src/components/Tasks/ListTasks.vue
+++ tasks/src/components/Tasks/ListTasks.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>{{title}}</h2>
 
-<form class="form form-inline" @submit.prevent="save()">
+<form class="form form-inline" @submit.prevent="onSubmit()">
     <input type="text" name="" id="" class="form-control" placeholder="Nome" v-model="task.name">
     <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
@@ -20,7 +20,7 @@
           <td>{{task.id}}</td>
           <td>{{task.name }}</td>
           <td>
-            <a href="" class="btn btn-info">Editar</a>
+            <a href="#" @click.prevent="edit(index)" class="btn btn-info">Editar</a>
             <a href="" class="btn btn-danger">Deletar</a>
           </td>
         </tr>
@@ -40,17 +40,40 @@ export default {
         id: '',
         name: ''
         },
+      updating: false,
+      updatingIndex: ''
     }
   },
   methods:{
+    onSubmit(){
+      if(this.updating){
+        return this.update();
+      }
+      return this.save()
+    },
     save(){
       this.task.id = this.tasks.length + 1;      
       this.tasks.push(this.task);
+      this.clearForm();      
 
+    },
+    edit(index){
+      this.task = this.tasks[index];
+
+      this.updatingIndex = index;
+
+      this.updating = true;
+    },
+    update(){
+      this.tasks[this.updatingIndex] = this.task;
+      this.updating = false;
+      this.clearForm();      
+    },
+    clearForm(){
       this.task = {
-        id: '',
-        name: ''
-      }
+          id: '',
+          name: ''
+        }
     }
   }
 }
