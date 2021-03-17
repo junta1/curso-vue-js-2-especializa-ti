986fd50704d9f7d950c47f99e1d840373af49507
Conceicao <rafael.conceicao@capgemini.com>
2021-03-17 20:38:58 -0300

    Add módulo resource.

---
+++ vue-resource-project/src/components/SearchCepComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 v-text="title"></h1>
+        <form @submit.prevent="onSubmit">
+            <input type="text" placeholder="Informe o CEP" v-model="cep">
+            <button type="submit">Buscar CEP</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Busca Cep com Vue JS',
+            cep: ''
+        }
+    },
+    methods: {
+        onSubmit(){
+            this.$http.get('https://api.postmon.com.br/v1/' + this.cep)
+            .then(response => {
+                console.log(response.body)
+            }, error => console.log(error))
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
