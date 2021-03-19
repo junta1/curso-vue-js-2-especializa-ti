57845ed8adc453f76e722740e7fdebe09f378b8a
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2021-03-18 23:45:51 -0300

    Adicionado preloader e tratamento de erros.

--- vue-resource-project/src/components/SearchCepComponent.vue
+++ vue-resource-project/src/components/SearchCepComponent.vue
@@ -5,6 +5,21 @@
             <input type="text" placeholder="Informe o CEP" v-model="cep">
             <button type="submit">Buscar CEP</button>
         </form>
+
+        <div v-if="error">
+            {{error}}
+        </div>
+
+        <div v-if="preloader">
+            <img src="../assets/preloader.gif" alt="Carregando..">
+        </div>
+
+        <div v-show="address.bairro">
+            <p><b>Bairro:</b>{{address.bairro}}</p>
+            <p><b>Cidade:</b>{{address.cidade}}</p>
+            <p><b>Logradouro:</b>{{address.logradouro}}</p>
+            <p><b>Estado:</b>{{address.estado}}</p>
+        </div>
     </div>
 </template>
 
@@ -13,15 +28,32 @@ export default {
     data() {
         return {
             title: 'Busca Cep com Vue JS',
-            cep: ''
+            cep: '',
+            address: {
+                bairro: ''
+            },
+            preloader: false,
+            error: ''
         }
     },
     methods: {
         onSubmit(){
-            this.$http.get('https://api.postmon.com.br/v1/' + this.cep)
+            this.reset()
+            this.preloader = true
+
+            this.$http.get('https://api.postmon.com.br/v1/cep/' + this.cep)
             .then(response => {
-                console.log(response.body)
-            }, error => console.log(error))
+                this.address = response.body
+            }, error => {
+                console.log(error)
+                this.error = "Cep errado!"
+            }).finally(() => {
+                this.preloader = false
+            })
+        },
+        reset(){
+            this.error = ''
+            this.address = {}
         }
     }
 }
