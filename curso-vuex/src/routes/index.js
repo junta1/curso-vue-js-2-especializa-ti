d496fbe2809aaa597f1a4ba602a04c2c2c7b325c
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2021-03-24 01:28:06 -0300

    Nova estrutura de rota.

--- curso-vuex/src/routes/index.js
+++ curso-vuex/src/routes/index.js
@@ -1,7 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import HomeComponent from '../components/home/HomeComponent'
 import TaskComponent from '../components/tasks/TaskComponent'
 
-export default [
+Vue.use(VueRouter);
+
+const routes = [
   {path: '/', component: HomeComponent, name: 'home'},
   {path: '/tasks', component: TaskComponent, name: 'tasks'},
 ]
+
+export default new VueRouter({
+  mode:'history',
+  routes
+})
