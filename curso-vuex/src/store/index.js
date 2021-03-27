930e0a8ea08937d7f76fa5156b46b0850544aad6
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2021-03-27 00:48:13 -0300

    Organizando projeto em módulos.

--- curso-vuex/src/store/index.js
+++ curso-vuex/src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import state from './state';
-import actions from './actions';
-import mutations from './mutations';
-import getters from './getters';
+import Tasks from './modules/Tasks/index';
+import Products from './modules/Products/index';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state,
-  actions,
-  mutations,
-  getters,
+  modules:{
+    Tasks,
+    Products
+  }
 });
---
+++ curso-vuex/src/store/modules/Products/mutations.js
@@ -0,0 +1,7 @@
+let mutations = {
+  ADD_PRODUCT_ID(state, products){
+    state.products.push(products);
+  }
+}
+
+export default mutations;
